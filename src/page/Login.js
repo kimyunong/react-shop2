46388f0508a8b95251252d57468cb923faaf75a3
kimyunong <money32132@naver.com>
2023-04-11 14:45:17 +0900

    commit03

--- src/page/Login.js
+++ src/page/Login.js
@@ -18,7 +18,7 @@ const loginUser=(event)=>{
   <Form onSubmit={(event)=>loginUser(event)}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="아무거나.." />
+        <Form.Control type="email" placeholder="Enter email - @포함해서 아무문자 입력" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -26,7 +26,7 @@ const loginUser=(event)=>{
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="아무거나.." />
+        <Form.Control type="password" placeholder="Password" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
